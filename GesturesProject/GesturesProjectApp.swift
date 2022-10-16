c0936d20e10a67bbe2044751467bae55b925900d
Elaine <ecf2@cin.ufpe.br>
2022-10-16 14:49:44 -0300

    Initial Commit

---
+++ GesturesProject/GesturesProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GesturesProjectApp.swift
+//  GesturesProject
+//
+//  Created by Elaine  Cruz on 16/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GesturesProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
